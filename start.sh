45781df269ac6c942bd2efbbdcf099169934a53a
manuel <mn05mayrhofer@gmail.com>
2023-12-17 20:25:08 +0100

    major fix now run in background

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo killall python3
 sudo killall php
-python3 -m http.server 8001 &
-sudo php -f index.php -S 192.168.1.228:80 &
+nohup python3 -m http.server 8001 &
+nohup sudo php -f index.php -S 192.168.1.228:80 &
