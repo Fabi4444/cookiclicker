c8f440c8bbe7eef1aacf5a54806d139ad82a20a2
manuel <mn05mayrhofer@gmail.com>
2023-12-28 15:49:56 +0100

    start.sh updated no compatible with apache2

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo killall python3
-sudo killall php
-nohup python3 -m http.server 8001 &
-nohup sudo php -f index.php -S 192.168.1.228:80 &
+#sudo killall python3
+#sudo killall php
+#nohup python3 -m http.server 8001 &
+#nohup sudo php -f index.php -S 192.168.1.228:80 &
+cp /home/linuxtyp/cookiclicker/index.php /var/www/html/
+cp /home/linuxtyp/cookiclicker/location.csv /var/www/html/
